dbfed3cdb821fd721f92fd9da38b210a308ff5cb
jidorta <ibandorta@gmail.com>
2020-09-11 10:12:09 +0200

    Agregar archivos de proyecto.

---
+++ BookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore_API.Controllers
+{
+
+   
+    
+ 
+
+    /// <summary>
+    /// Gets Values
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILoggerService _logger;
+
+        public HomeController(ILoggerService logger)
+        {
+            _logger = logger;
+        }
+
+        ///<summary>
+        /// Get a Value
+        ///</summary>
+        ///<return></return>
+        // GET: api/<HomeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Acceso Home Controller");
+            return new string[] { "value1", "value2" };
+        }
+
+
+
+        ///<summary>
+        /// Gets Values
+        ///</summary>
+        ///<param name="id"></param>
+        ///<return></return>
+        // GET api/<HomeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            _logger.LogDebug("Got a Value");
+            return "value";
+        }
+
+        // POST api/<HomeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _logger.LogIError("This is a error");
+        }
+
+        // PUT api/<HomeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<HomeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _logger.LogWarm("this is a warning");
+        }
+    }
+}
